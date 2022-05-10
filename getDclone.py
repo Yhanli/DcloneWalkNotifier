c059142ee7d7b598dd8a936e7c544748a50609f5
Yuhan Li <yhanl@hotmail.com>
2022-05-10 14:39:44 +1200

    add supervisor conf and crontab conf, run 3 time a minue

--- getDclone.py
+++ getDclone.py
@@ -5,25 +5,23 @@ from database import *
 from model import StatusController
 from telegram import *
 from datetime import datetime
+import time
 
 
 url = "https://diablo2.io/dclone_api.php?sk=p&sd=d&ladder=1&hc=2&plat_pc=1&plat_switch=0&plat_playstation=0&plat_xbox=0&region=0"
 
-
-#%%
-res = requests.get(url)
-data = json.loads(res.text)
-
-
-#%%
-print(datetime.now())
-
-# %%
 database = DcloneDB()
-for record in data:
-    status = StatusController(record)
-    if status.status_not_exist():
-        send_notification(status)
 
+#%%
+for i in range(3):
+    res = requests.get(url)
+    data = json.loads(res.text)
+
+    for record in data:
+        status = StatusController(record)
+        if status.status_not_exist():
+            send_notification(status)
+    time.sleep(20)
 
 # %%
+print(datetime.now())
